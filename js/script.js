23ebc23f22406776a3f5c6cdee22788ea64a03ee
WengranXiao <112583498+WengranXiao@users.noreply.github.com>
2023-04-04 21:54:46 -0400

    Delete notes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// DON'T CHANGE THE CONTENTS OF defaultEntries.
+
 const defaultEntries = [
   {
     title: 'Water',
@@ -17,64 +17,26 @@ const defaultEntries = [
   },
 ];
 
-// Every DOM element you need is already written to variables here. There's enough additional work to do.
+
 const memoryContainer = document.querySelector('#memory-container');
 const titleInput = document.querySelector("#title");
 const dateInput = document.querySelector("#date");
 const descriptionInput = document.querySelector("#description");
 const addMemoryButton = document.querySelector("#add-memory");
 
-// We also create the memoryList for you. If localStorage has a 'memory.list' item, it uses that,
-// otherwise it uses defaultEntries.
 let memoryList = localStorage.getItem('memory.list') ? JSON.parse(localStorage.getItem('memory.list')) : defaultEntries;
 
-/**
- * Takes the value of a date input and formats it in the manner required by the memory list.
- *
- * @param {string} dateString
- *   The date input value.
- * @return {string}
- *   The date formatted like "Monday, Mar 3, 2003".
- */
+
 const formatDateForMemory = (dateString) =>  {
   return new Date(dateString).toLocaleDateString(undefined, { weekday:"long", year:"numeric", month:"short", day:"numeric"})
 }
 
-/**
- * Converts a date string into a timestamp that accounts for the current time zone.
- *
- * @param {string} dateString
- *   The value of a date input.
- *
- * @return {number}
- *   A JS-friendly millisecond timestamp.
- */
 const localizedTimestamp = (dateString) => {
   return new Date(dateString).getTime() + new Date(dateString).getTimezoneOffset() * 60000
 }
 
-// YOUR WORK BEGINS HERE, finish the functions below.
-
-/**
- * Renders the memory list. Should be called on initial load
- * and anytime the memory list changes.
- */
 const renderMemories = () => {
-  // @todo
-  // - clear the memory container element
-  // - get a version of memoryList sorted by date
-  // - save the sorted memories to localStorage 'memory.list'
-  // - iterate through the date-sorted memories and each one should be added to memoryContainer as
-  /**
-   <div class="position-relative col-12 border border-secondary rounded my-3 p-3 bg-white">
-     <div class="d-flex">
-       <h3>THE-MEMORY-TITLE</h3>
-       <small class="px-1 text-muted align-self-center">THE-MEMORY-DATE-PROCESSED-BY-formatDateForMemory()</small>
-     </div>
-   <button data-date="THE-MEMORY-DATE-AS-TIMESTAMP" class="close-button">Ⓧ</button>
-   <p>THE-MEMORY-DESCRIPTION</p>
-   </div>
-   */
+
   memoryContainer.innerHTML = '';
   memoryList = memoryList.sort((a, b) => (a.date > b.date) ? 1 : -1);
   localStorage.setItem('memory.list',JSON.stringify(memoryList));
@@ -89,25 +51,11 @@ const renderMemories = () => {
   </div>`
   })
 }
-// @todo we need to render those memories 👆 on pageload (as well as when the button is clicked), that can happen
-// by adding a single line below 👇
+
 renderMemories();
 
-// This is the event handler that fires when you click
-// 'Add memory'
 addMemoryButton.addEventListener('click', (submit) => {
-  // @todo we need to validate titleInput, dateInput, descriptionInput
-  // If validation does not pass for a given input:
-  //   - The memory should not be created.
-  //   - The input with a validation problem gets the "is-invalid" class
-  //     added to it.
-  // VALIDATION CRITERIA:
-  //   titleInput: must not be empty, and must be one word (i.e. no spaces)
-  //   dateInput: must not be empty, and must not be a date used by another memory
-  //     @tip use localizedTimestamp() to convert the date input value into something
-  //     you can compare to a memory date field
-  //   descriptionInput: must not be empty
-  //&&&& 
+  
   memoryList.forEach((ele)=>{
     if (ele.date===localizedTimestamp(dateInput.value)){
       dateInput.classList.add("is-invalid");
@@ -119,15 +67,7 @@ addMemoryButton.addEventListener('click', (submit) => {
   if (dateInput.value===''){dateInput.classList.add("is-invalid")}else{dateInput.classList.remove("is-invalid")}
   if (descriptionInput.value===''){descriptionInput.classList.add("is-invalid")}else(descriptionInput.classList.remove("is-invalid"))
   
-  // If the form is resubmitted, any previously-validation-failing input that now
-  // passes should have the `is-invalid` class removed
-
-  // @todo if all validation passes
-  // - Add the new memory to the memoryList
-  //   @tip, the localizedTimestamp() function will convert the dateInput value to the proper timestamp.
-  // - Clear titleInput, dateInput, descriptionInput
-  // - Render the memory list on the page again.
-
+ 
   if (titleInput.classList[1]!='is-invalid'&&dateInput.classList[1]!='is-invalid'&& descriptionInput.classList[1]!='is-invalid'){
     memoryList.push({
       title: titleInput.value ,
@@ -138,31 +78,12 @@ addMemoryButton.addEventListener('click', (submit) => {
     dateInput.value='';
     descriptionInput.value='';
     renderMemories();
-  }
-    
+  }   
 })
-
-// This is the event handler that fires when you click
-// the Ⓧ remove button for a memory, it removes
-// a memory from the list
 memoryContainer.addEventListener('click', (e) => {
-  if (e.target.hasAttribute('data-date')) {
-    // We've done *almost* everything that is needed in this condition
-    // for you already. If this condition is reached, it means the Ⓧ remove button
-    // was clicked.
-    // @todo Remove the entry from memoryList with the .date property matching
-    // the value e.target's data-date attribute.
-    // @tip the data-date attribute is a string, and the items in memoryList
-    // store the dates as number, so we need to convert a number to string or
-    // vice versa to get a match.
-    memoryList.splice(memoryList.map(e=>e.date).indexOf(Number(e.target.getAttribute("data-date"))));
-
-    // @todo after removing the memory from the list we need to re-render the list on the page
-    // so the shows the current list (of which an item was just removed).
+  if (e.target.hasAttribute('data-date')) {  
+    memoryList.splice(memoryList.map(e=>e.date).indexOf(Number(e.target.getAttribute("data-date")))); 
     renderMemories();
-
-    // @tip only 2-4 lines need to be added inside this condition to accomplish
-    // what is needed.
   }
 })
 
